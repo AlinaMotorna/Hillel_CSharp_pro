54304d9c4221d0fb580f0eb408b6c417e1cf52c2
starv <starv@DESKTOP-26D6LIM>
2023-12-26 18:04:12 +0200

    new

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Class
+{
+    static public void Main()
+    {
+        double a, b, result;
+        char sym;
+
+        a = Convert.ToDouble(Console.ReadLine());
+
+        sym = Convert.ToChar(Console.ReadLine());
+
+        b = Convert.ToDouble(Console.ReadLine());
+
+        switch (sym)
+        {
+            case '+':
+                result = a + b;
+                Console.WriteLine(result);
+                break;
+            case '-':
+                result = a - b;
+                Console.WriteLine(result);
+                break;
+            case '*':
+                result = a * b;
+                Console.WriteLine(result);
+                break;
+            case '/':
+                if (b == 0)
+                {
+                    Console.WriteLine("Error");
+                }
+                else
+                {
+                    result = a / b;
+                }
+                break;
+        }
+    }
+}
