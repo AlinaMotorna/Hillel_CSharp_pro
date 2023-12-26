cdfd3e837021a3ce5bb80879fb07f7d65fbbbc68
starv <starv@DESKTOP-26D6LIM>
2023-12-26 18:09:52 +0200

    new

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,26 +6,27 @@ public class Class
     {
         double a, b, result;
         char sym;
-
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.WriteLine("Перше число:");
         a = Convert.ToDouble(Console.ReadLine());
-
+        Console.WriteLine("Операнд:");
         sym = Convert.ToChar(Console.ReadLine());
-
+        Console.WriteLine("Друге число:");
         b = Convert.ToDouble(Console.ReadLine());
 
         switch (sym)
         {
             case '+':
                 result = a + b;
-                Console.WriteLine(result);
+                Console.WriteLine($"=\n{result}");
                 break;
             case '-':
                 result = a - b;
-                Console.WriteLine(result);
+                Console.WriteLine($"=\n{result}");
                 break;
             case '*':
                 result = a * b;
-                Console.WriteLine(result);
+                Console.WriteLine($"=\n{result}");
                 break;
             case '/':
                 if (b == 0)
