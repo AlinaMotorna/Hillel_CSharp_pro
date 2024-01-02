4a998b7ba84cf044d6f5a927c115ec434b04a3e3
starv <starv@DESKTOP-26D6LIM>
2024-01-02 12:54:33 +0200

    new

---
+++ Lesson_4/Credit_card/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace e_mployee
+{
+    public class Program : Employee
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Зарплата працівників:");
+            Console.WriteLine(Plus(35000, 2500));
+            Console.WriteLine(Minus(35000, 2500));
+            Console.WriteLine(Rivno(35000, 2500));
+            Console.WriteLine(Proverka(35000, 2500));
+            Console.WriteLine(Nerivno(35000, 2500));
+            Console.WriteLine(Nerivno1(35000, 2500));
+            Console.ReadLine();
+
+
+        }
+    }
+}
