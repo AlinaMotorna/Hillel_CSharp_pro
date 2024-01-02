21d051557ad50272726557a3224352f6138d276a
starv <starv@DESKTOP-26D6LIM>
2024-01-02 05:55:38 +0200

    new

---
+++ Lesson_3/Imath/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    interface IMath
+    {
+        int Max() {  return int.MaxValue; }
+        int Min() {  return int.MinValue; }
+        float Avg() {  return 0; }
+        bool Search(int valueToSearch) {  return false; }
+    }
+    class MyArray : IMath
+    {
+        int[] myIntArray = new int[5] { 1, 2, 3, 4, 5 };
+        public void Show()
+        {
+            Console.WriteLine(myIntArray);
+        }
+    }
+   
+}
--- Lesson_3/Ioutput/Program.cs
+++ Lesson_3/Ioutput/Program.cs
@@ -1,28 +1,32 @@
-﻿using System.Reflection;
+﻿using Ioutput;
+using System.Reflection;
 namespace Ioutput
 {
-    interface IOutput
-    {
-        public void Show()
+    
+        interface IOutput
         {
-            string info = "info";
+            public void Show()
+            {
+                string info = "info";
+            }
+            public void Show(string info)
+            {
+                Console.WriteLine(info);
+            }
         }
-        public void Show(string info)
+        class MyArray : IOutput
         {
-            Console.Write(info);
+            int[] myIntArray = new int[5] { 1, 2, 3, 4, 5 };
+            public void Show()
+            {
+                Console.WriteLine(myIntArray);
+            }
+            public void Show(string info)
+            {
+                IOutput output = new MyArray();
+                output.Show(info);
+            }
         }
-    }
-    class MyArray : IOutput
-    {
-        int[] myIntArray = new int[5] { 1, 2, 3, 4, 5 };
-        public void Show()
-        {
-            Console.Write(myIntArray);
-        }
-        public void Show(string info)
-        {
-            IOutput output = new MyArray();
-            output.Show(info);
-        }
-    }
 }
+
+
