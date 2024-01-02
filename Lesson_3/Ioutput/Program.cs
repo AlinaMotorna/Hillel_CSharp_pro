2ad1c7ee6f3c0b55188137976c95e906dcb1026a
starv <starv@DESKTOP-26D6LIM>
2024-01-02 04:55:34 +0200

    new

--- Lesson_3/Ioutput/Program.cs
+++ Lesson_3/Ioutput/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿interface IOutput
+{
+    void Show()
+    {
+        string info = "info";
+    }
+    void Show(string info)
+    {
+        Console.WriteLine(info);
+    }
+}
+class MyArray : IOutput
+{
+    int[] myIntArray = new int[5] { 1, 2, 3, 4, 5 };
+    public void Show() 
+    {
+        Console.WriteLine(myIntArray);
+    }
+    void Show(string info) 
+    {
+        Console.WriteLine($"{myIntArray},{info} ");
+    }
+}
