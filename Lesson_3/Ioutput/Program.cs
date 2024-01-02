0870a5108fe317be4a54bdb1b357d90a470bd87a
starv <starv@DESKTOP-26D6LIM>
2024-01-02 08:01:37 +0200

    new

--- Lesson_3/Ioutput/Program.cs
+++ Lesson_3/Ioutput/Program.cs
@@ -2,31 +2,36 @@
 using System.Reflection;
 namespace Ioutput
 {
-    
-        interface IOutput
+    interface IOutput
+    {
+        public void Show()
         {
-            public void Show()
-            {
-                string info = "info";
-            }
-            public void Show(string info)
-            {
-                Console.WriteLine(info);
-            }
+            string info = "info";
         }
-        class MyArray : IOutput
+        public void Show(string info)
         {
-            int[] myIntArray = new int[5] { 1, 2, 3, 4, 5 };
-            public void Show()
-            {
-                Console.WriteLine(myIntArray);
-            }
-            public void Show(string info)
+            Console.WriteLine(info);
+        }
+    }
+    class MyArray : IOutput
+    {
+        int[] myIntArray = new int[5] { 1, 2, 3, 4, 5 };
+        public void Show()
+        {
+            Console.WriteLine(myIntArray);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Show(string info)
             {
                 IOutput output = new MyArray();
                 output.Show(info);
             }
         }
+    }
+
 }
-
-
