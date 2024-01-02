0f52c80587a843103e7b0278c121d98b1a6af38b
starv <starv@DESKTOP-26D6LIM>
2024-01-02 12:20:47 +0200

    new

--- Lesson_4/Town/City.cs
+++ Lesson_4/Town/City.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Town
 {
-    internal class City
+    public class City 
     {
+        public static int Plus(int zhitely, int zhitely2)
+        {
+            return zhitely + zhitely2;
+        }
+        public static int Minus(int zhitely, int zhitely2)
+        {
+            return zhitely - zhitely2;
+        }
+        public static string Rivno(int zhitely, int zhitely2)
+        {
+            if (zhitely == zhitely2)
+            {
+                return "Рівно";
+            }
+            else
+                return "Не рівно";
+        }
+        public static string Proverka(int zhitely, int zhitely2)
+        {
+            if (zhitely > zhitely2) return "Більше";
+            else
+                return "Меньше";
+
+        }
+        public static string Nerivno(int zhitely, int zhitely2)
+        {
+            if (zhitely != zhitely2) return "Рівно";
+            else
+                return "Не рівно";
+
+        }
+        public static string Nerivno1(int zhitely, int zhitely2)
+        {
+            if (zhitely.Equals(zhitely2)) return "Рівно";
+            else
+                return "Не рівно";
+
+        }
     }
 }
--- Lesson_4/Town/Program.cs
+++ Lesson_4/Town/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Town
+{
+    public class Program  : City
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Жителі:");
+            Console.WriteLine(Plus(30000, 2300));
+            Console.WriteLine(Minus(30000, 2000));
+            Console.WriteLine(Rivno(30000, 30000));
+            Console.WriteLine(Proverka(30000, 2300));
+            Console.WriteLine(Nerivno(30000, 2300));
+            Console.WriteLine(Nerivno1(30000, 30000));
+            Console.ReadLine();
+
+
+        }
+    }
+}
