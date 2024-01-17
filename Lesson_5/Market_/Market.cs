e6c0dccd739a7109fc72cbd1935b4ad8c7bcd3fc
starv <starv@DESKTOP-26D6LIM>
2024-01-18 00:40:10 +0200

    new

--- Lesson_5/Market_/Market.cs
+++ Lesson_5/Market_/Market.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Market_
 {
-    internal class Market
+    public class Market : IDisposable
     {
+
         string name = "Сільпо";
         string adress = "Вул...";
         string type = "Супермаркет";
@@ -16,6 +18,13 @@ namespace Market_
             Console.WriteLine($"Назва: {name}");
             Console.WriteLine($"Адреса: {adress}");
             Console.WriteLine($"Тип: {type}");
+        }
+        public void Dispose()
+        {
+            Console.WriteLine($"{name} has been disposed");
+            Console.WriteLine($"{adress} has been disposed");
+            Console.WriteLine($"{type} has been disposed");
+            GC.SuppressFinalize(this);
         }
     }
 }
